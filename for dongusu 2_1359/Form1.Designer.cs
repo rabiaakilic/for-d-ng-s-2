01ee946d234972a4e95304c4e05018b136d22a56
rabiaakilic <147382992+rabiaakilic@users.noreply.github.com>
2023-10-23 13:02:56 +0300

    Add files via upload

---
+++ for dongusu 2_1359/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace for_dongusu_2_1359
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.lbSayılar = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Location = new System.Drawing.Point(48, 55);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(129, 55);
+            this.btnGoster.TabIndex = 0;
+            this.btnGoster.Text = "GÖSTER";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // lbSayılar
+            // 
+            this.lbSayılar.FormattingEnabled = true;
+            this.lbSayılar.Location = new System.Drawing.Point(209, 55);
+            this.lbSayılar.Name = "lbSayılar";
+            this.lbSayılar.Size = new System.Drawing.Size(149, 238);
+            this.lbSayılar.TabIndex = 1;
+            this.lbSayılar.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbSayılar);
+            this.Controls.Add(this.btnGoster);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.ListBox lbSayılar;
+    }
+}
+
